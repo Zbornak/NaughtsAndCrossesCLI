1808da334d40a30cc7f64577e5271cb676b69cb6
zbornak <zbornak@protonmail.com>
2024-05-30 09:29:33 +0100

    Initial Commit

---
+++ Sources/NC.swift
@@ -0,0 +1,22 @@
+@available(macOS 10.15, *)
+@main
+struct NaughtsAndCrosses {
+    var choices = ["TL", "TM", "TR", "ML", "MM", "MR", "BL", "BM", "BR"]
+    var userChoice = ""
+    var opponentChoice = ""
+    
+    static func main() {
+        print("Hello Mark!")
+    }
+}
+
+func setBoard() {
+    
+}
+
+func startNewGame() {
+    setBoard()
+}
+
+
+
