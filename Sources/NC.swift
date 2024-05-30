6392741054f416ef912e502fa1fbc001bb463d97
zbornak <zbornak@protonmail.com>
2024-05-30 09:46:16 +0100

    Added playerselection function to adjust array

--- Sources/NC.swift
+++ Sources/NC.swift
@@ -1,22 +1,38 @@
 import Figlet
 
+var choices = [
+    ["TL", "TM", "TR"],
+    ["ML", "MM", "MR"],
+    ["BL", "BM", "BR"]
+]
+
+var userChoice = ""
+var opponentChoice = ""
+
 @main
 struct NaughtsAndCrosses {
-    var choices = ["TL", "TM", "TR", "ML", "MM", "MR", "BL", "BM", "BR"]
-    var userChoice = ""
-    var opponentChoice = ""
-    
     static func main() {
         Figlet.say("Naughts & Crosses")
         print("by zbornak (2024)")
+        print("")
+        startNewGame()
+        print("")
+        print("Make your choice:")
     }
 }
 
 func setBoard() {
-    
+    for choice in choices {
+        print(choice)
+    }
 }
 
 func startNewGame() {
+    setBoard()
+}
+
+func playerSelection(choice: String) {
+    
     setBoard()
 }
 
